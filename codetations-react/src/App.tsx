e7ace6def3722f8c4222f2a4d9d27e502aa62b92
Edward Misback <misback@cs.washington.edu>
2024-04-09 14:17:45 -0700

    Annotation addition, selection, and deletion working.

--- codetations-react/src/App.tsx
+++ codetations-react/src/App.tsx
@@ -1,12 +1,11 @@
 import React, { useEffect, useState } from 'react';
 // import logo from './logo.svg';
-import './App.css';
 import DocumentViewer from './DocumentViewer';
 import Annotation from './Annotation';
 import ReactDiffViewer from 'react-diff-viewer-continued';
 import Split from 'react-split'
 import './App.css'
-import CodeMirror, { Decoration, EditorState, EditorView, RangeSetBuilder, basicSetup } from '@uiw/react-codemirror';
+import CodeMirror, { Decoration, EditorState, EditorView, RangeSetBuilder, ViewPlugin, basicSetup } from '@uiw/react-codemirror';
 import { javascript } from '@codemirror/lang-javascript';
 
 
@@ -14,6 +13,7 @@ import { javascript } from '@codemirror/lang-javascript';
 import { useContext } from 'react';
 import { DocumentContext, DocumentProvider } from './DocumentContext';
 import { DiskStateContext, DiskStateProvider } from './DiskStateContext';
+import { hover } from '@testing-library/user-event/dist/hover';
 
 function App3(props: { documentContent: string, annotations: Annotation[] }) {
   // just render the document content with the annotations highlighted
@@ -61,37 +61,6 @@ function App3(props: { documentContent: string, annotations: Annotation[] }) {
 //     return builder.finish();
 //   };
 
-//   // Create a plugin to add the decoration to the view
-//   const highlightLinePlugin = ViewPlugin.fromClass(class {
-//     constructor(view: any) {
-//       this.decorations = [lineDecoration];
-//       this.update = this.update.bind(this);
-//       this.decorationsPlugin = Decoration.plugin(this.decorations);
-//       this.decorationsPlugin(view);
-//     }
-
-//     update(update) {
-//       const oldRanges = this.decorations.map(d => d.range);
-//       const updatedRanges = update.state.doc.lines.map((_, i) => ({
-//         anchor: i,
-//         head: i
-//       }));
-//       const diff = Decoration.diff(oldRanges, updatedRanges);
-//       this.decorations = this.decorations
-//         .filter(d => !diff.deletions.includes(d))
-//         .concat(diff.additions.map(range => Decoration.line({
-//           attributes: {
-//             style: 'background-color: red;'
-//           },
-//           range
-//         })));
-//       this.decorationsPlugin.update({
-//         decorations: this.decorations
-//       });
-//     }
-//   });
-
-
 //   // Editor state, initialized with decorations for annotations
 //   const [editorState, setEditorState] = useState(() => EditorState.create({
 //     doc: annotations[0]?.document || '',
@@ -106,6 +75,223 @@ function App3(props: { documentContent: string, annotations: Annotation[] }) {
 
 //   return <CodeMirror value={documentContent} height="200px" extensions={[javascript({ jsx: true })]} />;
 // }
+
+var mouseDown = 0;
+document.body.onmousedown = function() { 
+  ++mouseDown;
+}
+document.body.onmouseup = function() {
+  --mouseDown;
+}
+
+const HTMLEditor = (props: { documentContent: string, annotations: Annotation[], setAnnotations: (anns: Annotation[]) => void, hoveredAnnotation: Annotation|null, setHoveredAnnotation: (ann: Annotation|null) => void, selectedAnnotation: Annotation|undefined, setSelectedAnnotation: (ann: Annotation|undefined) => void}) => {
+  const { documentContent, annotations, setAnnotations,
+    hoveredAnnotation, setHoveredAnnotation,
+    selectedAnnotation, setSelectedAnnotation
+  } = props;
+  
+  // Just render the document content with the annotations highlighted.
+  // We break the document into characters and put a span on each character.
+  // We set the index on each span so that we can get the character index via the selection API when the user selects text in order to create an annotation.
+  // For each span, we adjust the highlight opacity based on the number of overlapping annotations at that index.
+  // We also add a hover handler that sets that annotation as "hovered" so it can be highlighted in the annotation list on the right.
+  // We also add a click handler that sets that annotation as "selected" so it can be scrolled to in the annotation list on the right.
+  // We also add a mouseleave handler that clears the "hovered" annotation.
+
+  // const [hoveredAnnotation, setHoveredAnnotation] = useState<Annotation | null>(null);
+  // const [selectedAnnotation, setSelectedAnnotation] = useState<Annotation | null>(null);
+  const [addStartEnd, setAddStartEnd] = useState<[number, number, boolean] | null>(null);
+  const [addTool, setAddTool] = useState<string>('colorPicker');
+  
+  const handleMouseEnter = (index: number) => {
+    const annotation = annotations.find(annotation => index >= annotation.start && index <= annotation.end);
+    if (!annotation) {
+      return;
+    }
+    setHoveredAnnotation(annotation);
+  };
+
+  const handleMouseLeave = () => {
+    setHoveredAnnotation(null);
+  };
+
+  const handleClick = (index: number) => {
+    const annotation = annotations.find(annotation => index >= annotation.start && index <= annotation.end);
+    if (!annotation) {
+      setSelectedAnnotation(undefined)
+      return;
+    }
+    setSelectedAnnotation(annotation);
+  };
+
+  const numOverlappingAnnotations = (index: number) => {
+    return annotations.filter(annotation => index >= annotation.start && index <= annotation.end).length;
+  }
+
+  const maxOverlappingAnnotations = [...new Array(documentContent.length)].reduce((acc, _, index) => {
+    const numOverlapping = numOverlappingAnnotations(index);
+    return numOverlapping > acc ? numOverlapping : acc;
+  }, 0);
+
+  const getOpacity = (index: number) => {
+    const maxOpacity = 1;
+    const minOpacity = .3;
+    // handle case of no overlapping annotations
+    if (maxOverlappingAnnotations === 0) {
+      return maxOpacity;
+    }
+    const numOverlapping = numOverlappingAnnotations(index);
+    return numOverlapping === 0 ? 0 : minOpacity + (maxOpacity - minOpacity) * (numOverlapping / maxOverlappingAnnotations);
+  };
+
+  const handleSelection = (e: any) => {
+    
+    const selection = window.getSelection();
+    if (!selection) {
+      clearSelection();
+      return;
+    }
+    if (!selection.anchorNode?.parentElement?.dataset.index || !selection.focusNode?.parentElement?.dataset.index) {
+      clearSelection();
+      return
+    }
+    // Get the data-index of the start and end nodes of the selection
+    const anchorIndex = parseInt(selection.anchorNode?.parentElement?.dataset.index || '0');
+    const focusIndex = parseInt(selection.focusNode?.parentElement?.dataset.index || '0');
+    const start = Math.min(anchorIndex, focusIndex);
+    const end = Math.max(anchorIndex, focusIndex);
+    const isCaret = selection.type === 'Caret';
+    console.log('Selection:', start, end);
+    setAddStartEnd([start, end, isCaret]);
+  }
+
+  const clearSelection = () => {
+    setAddStartEnd(null);
+  }
+
+  const handleAddAnnotationClick = () => {
+    if (!addStartEnd) {
+      console.error('Error: no selection');
+      return;
+    }
+    let [start, end] = addStartEnd;
+    end = end + 1;  // HACK fix off-by-one error
+    const newAnnotation: Annotation = {
+      start,
+      end,
+      document: documentContent,
+      tool: addTool,
+      metadata: {},
+      original: {
+        document: documentContent,
+        start,
+        end
+      }
+    };
+    console.log('Adding annotation:', newAnnotation);
+    setAnnotations([...annotations, newAnnotation]);
+    clearSelection();
+  }
+
+  /* TODO handle case where the selected character is the end of the line (can't do inline-block) */
+
+  return (
+    <div>
+      <div style={{ display: 'flex' }}>
+        <div style={{ flex: 1 }}>
+          <h1>Document Content</h1>
+          <div className="html-annotator" style={{ whiteSpace: 'pre-wrap', fontFamily: 'Source Code Pro' }}>
+            {documentContent.split('').map((char, index) => {
+              let style = {}
+              // We want to highlight the character based on the number of overlapping annotations
+              style = { backgroundColor: `rgba(255, 255, 0, ${getOpacity(index)})` };
+              // if character is part of a currently hovered annotation, highlight in light gray
+              if (hoveredAnnotation && index >= hoveredAnnotation.start && index <= hoveredAnnotation.end) {
+                style = { backgroundColor: 'lightgray' };
+              }
+
+              // if the character is part of a currently selected annotation, highlight in green
+              if (selectedAnnotation && index >= selectedAnnotation.start && index <= selectedAnnotation.end) {
+                style = { backgroundColor: 'rgba(0, 255, 0, 0.5)' };
+              }
+              // If the character is part of a currently selected addStartEnd, highlight it in a different color
+              if (addStartEnd && index >= addStartEnd[0] && index <= addStartEnd[1]) {
+                style = { backgroundColor: 'rgba(0, 255, 0, 0.5)' };
+                // if it's a caret, just add a border to the left side
+                if (addStartEnd[2]) {
+                  style = { borderLeft: '2px solid green', "box-sizing": 'border-box', width: '.6em', display:'inline-block'};
+                }
+              }
+              return (
+                <span
+                  key={index}
+                  data-index={index}
+                  style={style}
+                  onMouseUp={handleSelection }
+                  onMouseEnter={(e) => ( handleSelection(e), handleMouseEnter(index))}
+                  onMouseLeave={e => handleMouseLeave()}
+                  onClick={() => handleClick(index)}
+                >
+                  {char}
+                </span>
+              );
+            })}
+          </div>
+        </div>
+        {/* <div style={{ flex: 1 }}>
+          <h1>Annotations</h1>
+          {annotations.map((annotation, index) => (
+            <div key={index} style={{ backgroundColor: selectedAnnotation === annotation ? 'lightblue' : 'transparent' }}>
+              <div>Start: {annotation.start}</div>
+              <div>End: {annotation.end}</div>
+              <div>Document: {annotation.document}</div>
+              <div>Tool: {annotation.tool}</div>
+              <div>Metadata: {JSON.stringify(annotation.metadata)}</div>
+            </div>
+          ))}
+        </div> */}
+      </div>
+      <div>
+        <h1>Add Annotation</h1>
+        <div>Tool: &nbsp;
+          {/* select with dropdown */}
+          {/* <input type="text" value={addTool} onChange={e => setAddTool(e.target.value)} /> */}
+          <select value={addTool} onChange={e => setAddTool(e.target.value)}>
+            <option value="colorPicker">Color Picker</option>
+          </select>
+          
+        </div>
+        <div>Start: {addStartEnd?.[0]}</div>
+        <div>End: {addStartEnd?.[1]}</div>
+        <button onClick={handleAddAnnotationClick}>Add</button>
+      </div>
+      {/* delete annotation button if there's a selected annotation */}
+      {selectedAnnotation && <div>
+        <h1>Delete Annotation</h1>
+        <button onClick={() => setAnnotations(annotations.filter(annotation => annotation !== selectedAnnotation))}>Delete</button>
+      </div>}
+      
+      {/* Hovered annotation */}
+      {/* <div>
+        <h1>Hovered Annotation</h1>
+        <div>Start: {hoveredAnnotation?.start}</div>
+        <div>End: {hoveredAnnotation?.end}</div>
+        <div>Document: {hoveredAnnotation?.document}</div>
+        <div>Tool: {hoveredAnnotation?.tool}</div>
+        <div>Metadata: {JSON.stringify(hoveredAnnotation?.metadata)}</div>
+      </div> */}
+      {/* Selected annotation */}
+      {/* <div>
+        <h1>Selected Annotation</h1>
+        <div>Start: {selectedAnnotation?.start}</div>
+        <div>End: {selectedAnnotation?.end}</div>
+        <div>Document: {selectedAnnotation?.document}</div>
+        <div>Tool: {selectedAnnotation?.tool}</div>
+        <div>Metadata: {JSON.stringify(selectedAnnotation?.metadata)}</div>
+      </div> */}
+    </div>
+  );
+}
 
 function useDocument() {
   const context = useContext(DocumentContext);
@@ -144,7 +330,7 @@ const ColorPicker: React.FC<AnnotationEditorProps> = (props) => {
   );
 }
 
-function AnnotationEditorContainer(props: { value: Annotation, setValue: (value: AnnotationUpdate) => void }) {
+function AnnotationEditorContainer(props: { value: Annotation, setValue: (value: AnnotationUpdate) => void, hoveredAnnotation: Annotation|null, selectedAnnotation: Annotation|undefined }) {
   const { value, setValue } = props;
 
   type ToolTypes = {
@@ -154,18 +340,21 @@ function AnnotationEditorContainer(props: { value: Annotation, setValue: (value:
   const toolTypes : ToolTypes  = {
     colorPicker: ColorPicker,
   }
+  const style = {
+    backgroundColor: props.selectedAnnotation === value ? 'lightgreen' : props.hoveredAnnotation === value ? 'lightgray' : 'transparent'
+  }
 
   return (
-    <div>
+    <div style={ style }>
       <h2>Annotation</h2>
-      <div>Start: {value.start}</div>
+      {/* <div>Start: {value.start}</div>
       <div>End: {value.end}</div>
       <div>Document: {value.document}</div>
       <div>Tool: {value.tool}</div>
       <div>Metadata: {JSON.stringify(value.metadata)}</div>
       <div>Original Document: {value.original.document}</div>
       <div>Original Start: {value.original.start}</div>
-      <div>Original End: {value.original.end}</div>
+      <div>Original End: {value.original.end}</div> */}
       <div>Editor:</div>
       {toolTypes[value.tool]?.({
         value,
@@ -212,6 +401,8 @@ function Main() {
   const { diskState, setDiskState } = useDiskState();
   const [continuousRetag, setContinuousRetag] = useState(false);
   const [documentOutOfDate, setDocumentOutOfDate] = useState(false);
+  const [hoveredAnnotation, setHoveredAnnotation] = useState<Annotation | null>(null);
+  const [selectedAnnotation, setSelectedAnnotation] = useState<Annotation | undefined>(undefined);
   const annotations = diskState?.annotations;
 
   const setAnnotation = (index: number, annotationUpdate: AnnotationUpdate) => {
@@ -285,12 +476,16 @@ function Main() {
     console.error("Error: annotations couldn't be read", diskState);
     return <div>Error: annotations couldn't be read</div>;
   }
+
+  const setAnnotations = (anns: Annotation[]) => {
+    setDiskState({ annotations: anns });
+  }
   
   return (
     <DiskStateProvider stateURI='example/.sample.txt.ann.json' serverUrl='ws://localhost:3002'>
       <DocumentProvider serverUrl="ws://localhost:3002" documentURI='example/sample.txt'>
         <Split className="split">
-          {/* <App2 documentContent={documentContent} annotations={ annotations }></App2> */}
+          <HTMLEditor documentContent={documentContent} annotations={annotations} setAnnotations={ setAnnotations }  hoveredAnnotation={hoveredAnnotation} selectedAnnotation={selectedAnnotation} setHoveredAnnotation={setHoveredAnnotation} setSelectedAnnotation={setSelectedAnnotation}></HTMLEditor>
     <div className="App">
       {/* Document path to open */}
       <div>Document URI: &nbsp;
@@ -321,7 +516,7 @@ function Main() {
         {/* list of annotations */}
         <h1>Annotations</h1>
         {annotations?.map((annotation, index) => (
-          <AnnotationEditorContainer value={annotation} setValue={(a) => setAnnotation(index, a)} key={index} />
+          <AnnotationEditorContainer value={annotation} setValue={(a) => setAnnotation(index, a)} key={index} hoveredAnnotation={hoveredAnnotation} selectedAnnotation={selectedAnnotation} />
         ))}
             </div>
         </div>
