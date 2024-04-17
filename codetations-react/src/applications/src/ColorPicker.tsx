f6042333e60d63c078ba7bfcbb337b72575f1be1
Edward Misback <misback@cs.washington.edu>
2024-04-17 00:56:33 -0700

    Compress color picker.

--- codetations-react/src/applications/src/ColorPicker.tsx
+++ codetations-react/src/applications/src/ColorPicker.tsx
@@ -3,12 +3,9 @@ import AnnotationEditorProps from '../../AnnotationEditorProps';
 import NoCodeTool from '../lib/NoCodeTool/NoCodeTool';
 
 const ColorPicker: React.FC<AnnotationEditorProps> = (props) => {
-  console.log('color', JSON.stringify(props.utils.getText()));
-  return (
-    <input type="color"
-      value={props.utils.getText()}
-      onChange={e => props.utils.setText(e.target.value)} />
-  );
+  return (<input type="color"
+    value={props.utils.getText()}
+    onChange={e => props.utils.setText(e.target.value)} />);
 }
 
 export default ColorPicker;
