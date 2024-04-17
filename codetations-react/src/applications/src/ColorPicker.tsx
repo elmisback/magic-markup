d6593775cb7bf2de13f2605adaaec158129f6853
Edward Misback <misback@cs.washington.edu>
2024-04-17 00:57:22 -0700

    Remove nocodetool reference from color picker.

--- codetations-react/src/applications/src/ColorPicker.tsx
+++ codetations-react/src/applications/src/ColorPicker.tsx
@@ -1,7 +1,4 @@
 import AnnotationEditorProps from '../../AnnotationEditorProps';
-
-import NoCodeTool from '../lib/NoCodeTool/NoCodeTool';
-
 const ColorPicker: React.FC<AnnotationEditorProps> = (props) => {
   return (<input type="color"
     value={props.utils.getText()}
