cfab7b963a0c6b9f53a66d7f7b019e549af1eb0d
erikv05 <erikv05@uw.edu>
2024-07-10 18:50:21 -0400

    Add runCodeSegment tool

--- codetations-react/src/tools.tsx
+++ codetations-react/src/tools.tsx
@@ -1,28 +1,60 @@
-import { AnnotationEditorProps } from './App'
-import React from 'react';
-
-/* TODO: Define additional tools as React components here. */
+import { AnnotationEditorProps } from "./App";
+import React from "react";
+import { useState } from "react";
 
 const ColorPicker: React.FC<AnnotationEditorProps> = (props) => {
   return (
-    <input type="color"
+    <input
+      type="color"
       value={props.utils.getText()}
-      onChange={e => props.utils.setText(e.target.value)} />
+      onChange={(e) => props.utils.setText(e.target.value)}
+    />
   );
-}
+};
 
 const Comment: React.FC<AnnotationEditorProps> = (props) => {
   return (
     <textarea
       value={props.value.metadata.comment || ""}
-      onChange={e =>
-        props.utils.setMetadata({comment: e.target.value})} />
+      onChange={(e) => props.utils.setMetadata({ comment: e.target.value })}
+    />
   );
-}
-  
+};
+
+const RunCodeSegment: React.FC<AnnotationEditorProps> = (props) => {
+  const [response, setResponse] = useState(null);
+  const [error, setError] = useState("");
+
+  function runCode(): void {
+    try {
+      const result = new Function(props.utils.getText())();
+      setResponse(result);
+      setError("");
+    } catch (e) {
+      setError(e instanceof Error ? e.message : String(e));
+      setResponse(null);
+    }
+  }
+
+  return (
+    <div>
+      {error && <div style={{ color: "red" }}>{error}</div>}
+      {response && <div>{response}</div>}
+      <input
+        type="text"
+        value={props.utils.getText()}
+        onChange={(e) => props.utils.setText(e.target.value)}
+      />
+      <button onClick={runCode}>Run Highlighted Code</button>
+    </div>
+  );
+};
+
+/* TODO: Define additional tools as React components here. */
 
 /* TODO: Add all tools to be used here. */
 export const tools = {
-    comment: Comment,
-    colorPicker: ColorPicker
-}
+  comment: Comment,
+  colorPicker: ColorPicker,
+  runCodeSegment: RunCodeSegment,
+};
