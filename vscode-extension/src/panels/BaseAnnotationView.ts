71c147cf00911998de1dae4f567d6315c6258948
Edward Misback <misback@cs.washington.edu>
2025-03-12 06:54:09 -0700

    Fix image upload

--- vscode-extension/src/panels/BaseAnnotationView.ts
+++ vscode-extension/src/panels/BaseAnnotationView.ts
@@ -88,7 +88,7 @@ export abstract class BaseAnnotationView {
         <head>
           <meta charset="UTF-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-          <meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src ${webview.cspSource}  https://*.googleapis.com 'unsafe-eval'; script-src 'nonce-${nonce}' http://localhost:* 'unsafe-eval'; connect-src 'self' http://localhost:*; font-src https://fonts.gstatic.com; ">
+          <meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src ${webview.cspSource}  https://*.googleapis.com 'unsafe-eval'; script-src 'nonce-${nonce}' http://localhost:* 'unsafe-eval'; connect-src 'self' http://localhost:*; font-src https://fonts.gstatic.com; img-src * data:;">
           <link rel="stylesheet" type="text/css" href="${stylesUri}">
           <title>Annotations</title>
         </head>
