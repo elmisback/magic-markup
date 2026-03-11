e0708f2f8eaf89609527a72464c46a28cd621250
Edward Misback <misback@cs.washington.edu>
2026-03-11 00:56:10 -0400

    Use response.stream instead of response.text in retag. Fixes #33.
    Tested working on at least one simple file (comment-test.js). Not a fix for issue 30, so quicksort.js is still borked.

--- vscode-extension/src/server/retag.ts
+++ vscode-extension/src/server/retag.ts
@@ -109,7 +109,7 @@ The object must look like: {1: <code>, 2: <number>, 3: <number>, 4: <number>}
 
 The answer to 1 should be a code string only, without markdown formatting or extra notes.`;
 
-import vscode from 'vscode';
+import vscode, { LanguageModelTextPart, LanguageModelToolCallPart } from 'vscode';
 
 async function copilotPromptGPTForJSON(t: string) {
   const craftedPrompt = [
@@ -132,9 +132,13 @@ async function copilotPromptGPTForJSON(t: string) {
     const response = await model.sendRequest(craftedPrompt, {}, new vscode.CancellationTokenSource().token);
     console.log('Got response:', response);
     let fullResponse = '';
-    for await (const chunk of response.text) {
-      console.debug('Got chunk:', chunk);
-      fullResponse += chunk;
+    for await (const chunk of response.stream) {
+      if (chunk instanceof LanguageModelTextPart) {
+        console.debug("TEXT", chunk);
+        fullResponse += chunk.value;
+      } else if (chunk instanceof LanguageModelToolCallPart) {
+        console.debug("TOOL CALL", chunk);
+      }
     }
     return fullResponse;
     // TODO figure out how to get the completion here
